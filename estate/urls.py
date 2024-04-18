34459eb1aa1c04171bd1a20d52c5f372208c819b
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-04-18 16:25:58 +0300

    Made home page

--- estate/urls.py
+++ estate/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from .views import (
+    index
+)
 
-urlpatterns = []
+urlpatterns = [
+    path("", index, name="index"),
+
+]
 
 app_name = "estate"
--- estate/views.py
+++ estate/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import (
+    House,
+    Realtor,
+)
 
-# Create your views here.
+
+def index(request):
+    num_houses = House.objects.count()
+    num_realtors = Realtor.objects.count()
+
+    # num_visits = request.session.get("num_visits", 0)
+    # request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_houses": num_houses,
+        "num_realtors": num_realtors,
+        # "num_visits": num_visits + 1,
+    }
+
+    return render(request, "estate/index.html", context=context)
