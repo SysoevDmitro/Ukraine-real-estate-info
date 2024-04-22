d7a0a55b19d01d536d5febd167bd1845b5e06ad4
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-04-23 01:22:33 +0300

    Some changes

--- estate/views.py
+++ estate/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import HouseSearchForm
+from .forms import RealtorCreationForm, RealtorUpdateForm, RealtorUsernameSearchForm, HouseSearchForm
 
 from .models import (
     House,
@@ -32,6 +32,25 @@ class RealtorListView(generic.ListView):
     model = Realtor
     paginate_by = 5
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(RealtorListView, self).get_context_data(**kwargs)
+
+        username = self.request.GET.get("username", "")
+
+        context["search_form"] = RealtorUsernameSearchForm(
+            initial={"username": username})
+        return context
+
+    def get_queryset(self):
+        queryset = Realtor.objects.all()
+        form = RealtorUsernameSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"])
+
+        return super().get_queryset()
+
 
 class RealtorDetailView(generic.DetailView):
     model = Realtor
@@ -40,12 +59,14 @@ class RealtorDetailView(generic.DetailView):
 class RealtorCreateView(LoginRequiredMixin, generic.CreateView):
     model = Realtor
     fields = "__all__"
+    form = RealtorCreationForm
     success_url = reverse_lazy("estate:realtor-list")
 
 
 class RealtorUpdateView(LoginRequiredMixin, generic.UpdateView):
     model = Realtor
     fields = "__all__"
+    form = RealtorUpdateForm
     success_url = reverse_lazy("estate:realtor-list")
 
 
@@ -60,14 +81,24 @@ class HouseListView(generic.ListView):
     paginate_by = 3
     queryset = House.objects.all()
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(HouseListView, self).get_context_data(**kwargs)
+
+        price = self.request.GET.get("price", "")
+
+        context["search_price_form"] = HouseSearchForm(
+            initial={"price": price})
+        return context
+
     def get_queryset(self):
-        houses = House.objects.all()
-        for house in houses:
-            if house.area != 0:
-                house.price_per_area = round(house.price / house.area)
-            else:
-                house.price_per_area = None
-        return houses
+        queryset = House.objects.all()
+        form = HouseSearchForm(self.request.GET)
+
+        if form.is_valid():
+            return queryset.filter(
+                price__icontains=form.cleaned_data["price"])
+
+        return super().get_queryset()
 
 
 class HouseDetailView(generic.DetailView):
