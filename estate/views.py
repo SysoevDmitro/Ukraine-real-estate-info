bcfdba76b7b15b440bd037978dfbdbda63e87c0c
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-04-23 15:41:48 +0300

    some chenges

--- estate/views.py
+++ estate/views.py
@@ -16,13 +16,9 @@ def index(request):
     num_houses = House.objects.count()
     num_realtors = Realtor.objects.count()
 
-    # num_visits = request.session.get("num_visits", 0)
-    # request.session["num_visits"] = num_visits + 1
-
     context = {
         "num_houses": num_houses,
         "num_realtors": num_realtors,
-        # "num_visits": num_visits + 1,
     }
 
     return render(request, "estate/index.html", context=context)
