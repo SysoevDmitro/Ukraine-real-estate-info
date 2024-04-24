fc2e9f17376a65021c998abb82766b2880bb825d
Dmitro Sysoev <dimasysoev20000@gmail.com>
2024-04-24 21:51:29 +0300

    s

--- real_estate_info/settings.py
+++ real_estate_info/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != False
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "ukraine-real-estate-info.onrender.com"]
 
 
 # Application definition
